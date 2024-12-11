57eded973a92f728c41c4f34c93157455f7f5196
caeyon <shallyshin@naver.com>
2024-12-11 17:23:35 +0900

    ✨ LazyColumn 사용

--- app/src/main/java/kr/ac/kumoh/s20220625/s24w1403retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20220625/s24w1403retrofit/MainActivity.kt
@@ -7,16 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import kr.ac.kumoh.s20220625.s24w1403retrofit.ui.theme.S24W1403RetrofitTheme
 
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.lifecycle.viewmodel.compose.viewModel
+
+import androidx.compose.foundation.lazy.items
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,7 +37,7 @@ fun MainScreen(viewModel: SongViewModel = viewModel()) { //viewModel()에 오류
     val songList by viewModel.songList.observeAsState(emptyList())
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-        SongResult(
+        SongList(
             list = songList,
             modifier = Modifier.padding(innerPadding)
         )
@@ -43,10 +45,18 @@ fun MainScreen(viewModel: SongViewModel = viewModel()) { //viewModel()에 오류
 }
 
 @Composable
-fun SongResult(list: List<Song>, modifier: Modifier) {
-    Column(
-        modifier
+fun SongList(list: List<Song>, modifier: Modifier) {
+    LazyColumn(
+        modifier,
     ) {
-        Text(list.toString())
+//        item {
+//            Text("Header")
+//        }
+        items(list) { song ->
+            Text(list.toString())
+        }
+//        item {
+//            Text("Footer") //이렇게 위 아래에 텍스트(아이템) 넣을 수 있음
+//        }
     }
 }
