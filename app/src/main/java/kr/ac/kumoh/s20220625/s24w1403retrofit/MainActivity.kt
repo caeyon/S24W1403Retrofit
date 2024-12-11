4e680861a8ab4e2425fa5398c028bdfcf52033d2
caeyon <shallyshin@naver.com>
2024-12-11 17:32:11 +0900

    ✨ LazyColumn 사용

--- app/src/main/java/kr/ac/kumoh/s20220625/s24w1403retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20220625/s24w1403retrofit/MainActivity.kt
@@ -4,8 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Scaffold
@@ -19,6 +23,9 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.lifecycle.viewmodel.compose.viewModel
 
 import androidx.compose.foundation.lazy.items
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -48,6 +55,8 @@ fun MainScreen(viewModel: SongViewModel = viewModel()) { //viewModel()에 오류
 fun SongList(list: List<Song>, modifier: Modifier) {
     LazyColumn(
         modifier,
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(horizontal = 8.dp)
     ) {
 //        item {
 //            Text("Header")
@@ -59,4 +68,27 @@ fun SongList(list: List<Song>, modifier: Modifier) {
 //            Text("Footer") //이렇게 위 아래에 텍스트(아이템) 넣을 수 있음
 //        }
     }
+}
+
+@Composable
+fun SongItem(song: Song) {
+    Column (
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color(0xffffffcc))
+            .padding(16.dp)
+    ) {
+        TextTitle(song.title)
+        TextSinger(song.singer)
+    }
+}
+
+@Composable
+fun TextTitle(title: String) {
+    Text(title, fontSize = 30.sp)
+}
+
+@Composable
+fun TextSinger(singer: String) {
+    Text(singer, fontSize = 30.sp)
 }
---
+++ app/src/main/java/kr/ac/kumoh/s20220625/s24w1403retrofit/SongApiConfig.kt
@@ -0,0 +1,6 @@
+package kr.ac.kumoh.s20220625.s24w1403retrofit
+
+object SongApiConfig {
+    const val SERVER_URL = "https://knfyxpvnztfphgowvwem.supabase.co/rest/v1/"
+    const val API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtuZnl4cHZuenRmcGhnb3d2d2VtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzM4NDAwMTIsImV4cCI6MjA0OTQxNjAxMn0.NNnKEItMC4bzrVt4eBYbP_q2_n1MDCYcdoF8ObF2-Zo"
+}
