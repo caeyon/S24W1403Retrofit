f81ab7285365f0f92be2c96500a22b21d385c76d
caeyon <shallyshin@naver.com>
2024-12-13 17:32:37 +0900

    🧐 Item 클릭 가사 출력

--- app/src/main/java/kr/ac/kumoh/s20220625/s24w1403retrofit/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20220625/s24w1403retrofit/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
@@ -32,9 +34,12 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil3.compose.AsyncImage
@@ -84,7 +89,13 @@ fun SongList(list: List<Song>, modifier: Modifier) {
 
 @Composable
 fun SongItem(song: Song) {
+    var (expanded, setExpanded) = remember { mutableStateOf(false) }
+
     Card (
+        modifier = Modifier
+            .clickable {
+                setExpanded(!expanded)
+            },
         elevation = CardDefaults.cardElevation(8.dp)
     ) {
         Row(
@@ -112,7 +123,18 @@ fun SongItem(song: Song) {
                 TextSinger(song.singer)
             }
         }
-}
+        AnimatedVisibility(
+            visible = expanded,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            song.lyrics?.let {
+                Text(
+                    it.replace("\n", "\n"),
+                    textAlign = TextAlign.Center,
+                )
+            }
+        }
+    }
 
 
 }
